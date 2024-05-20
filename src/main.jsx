fe183c033857dd25ffe08f524a51201e57ef8244
KarthikSbrshB <karthiksb.programming@gmail.com>
2024-05-20 21:51:19 +0530

    modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from '../../gemini-clone/src/App'
 import './index.css'
-import ContextProvider from './context/Context.jsx'
+import ContextProvider from '../../gemini-clone/src/context/Context.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ContextProvider>
